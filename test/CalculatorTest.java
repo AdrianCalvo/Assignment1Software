3b7545a54ee01942221143eeea911ce8eec53b5a
AdrianCalvo <ac222ne@student.lnu.se>
2015-11-11 20:18:15 +0100

    RED

---
+++ test/CalculatorTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import assignment1software.Calculator;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author adri
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void sumInteger_SumTwoIntegers_ReturnValueEqual5() {
+        Calculator cal = new Calculator();
+        int result = cal.sumInteger(2, 3);
+        assertEquals(5, result);
+    }
+}
