baa0b87d45255c51343cb5683cfc799ea38c045d
AdrianCalvo <ac222ne@student.lnu.se>
2015-11-11 20:21:55 +0100

    GREEN

---
+++ src/assignment1software/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1software;
+
+/**
+ *
+ * @author adri
+ */
+public class Calculator {
+    
+    public int sumInteger(int a, int b){
+        a = 2;
+        b = 3;
+        int result = a + b;
+        return result;
+    }
+}
