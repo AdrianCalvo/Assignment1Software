7248709f39f2aca523addbd185132b6701120e4e
AdrianCalvo <ac222ne@student.lnu.se>
2015-11-11 21:22:15 +0100

    BLUE

--- src/assignment1software/Calculator.java
+++ src/assignment1software/Calculator.java
@@ -30,7 +30,7 @@ public class Calculator {
     public int factorial(int a){
         int number = 1;
         for (int i = 1; i <= a; i++) {
-            number = number * i;
+            number *= i;
         }
         return number;
         
